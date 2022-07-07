e7a223b11bbfb092b0db96b3b5d7af06d9112c11
Eduardo Valencio <108006820+projEduardoValencio@users.noreply.github.com>
2022-07-07 15:12:45 -0300

    update: home page

--- lib/src/views/home_page.dart
+++ lib/src/views/home_page.dart
@@ -1,8 +1,26 @@
+import 'package:agenda_de_contatos/src/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //VARIAVEIS====================================================================
+  ContatctHelper cHelper = ContatctHelper();
+
+  //OVERRIDES===================================================================
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    cHelper.getAllContacts();
+  }
+
+  //BUILDER=====================================================================
   @override
   Widget build(BuildContext context) {
     return Scaffold(
